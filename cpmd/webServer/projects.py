485bebbc85b2ec8ef60c7c98cb0c4744dcd33888
Stephen Gaito <stephen@perceptisys.co.uk>
2022-03-02 20:35:53 +0000

    added project add and remove interfaces

--- cpmd/webServer/projects.py
+++ cpmd/webServer/projects.py
@@ -2,6 +2,8 @@
 
 import yaml
 
+from cpinterfaces.python.projectPath import ProjectPath
+
 def implementProjectInterfaces(self) :
 
   @self.app.get_projects
@@ -11,3 +13,15 @@ def implementProjectInterfaces(self) :
     for aProject, projectDetails in projects.items() :
       projectsDict[aProject] = projectDetails['path']
     return projectsDict
+
+  @self.app.post_project_add
+  async def post_project_add_impl(projectPath: ProjectPath) :
+    projects = self.managers.projects.projectData['projects']
+    if projectPath.projectName not in projects :
+      projects[projectPath.projectName] = projectPath.projectDir
+
+  @self.app.post_project_remove
+  async def post_project_remove_impl(projectPath: ProjectPath) :
+    projects = self.managers.projects.projectData['projects']
+    if projectPath.projectName in projects :
+      del projects[projectPath.projectName]
